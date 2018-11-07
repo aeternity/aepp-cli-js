5f9fe1752cbebbf2f2f4fa692a4066a7c53d9e01
nduchak <nduchak@n-ix.com>
2018-11-07 13:38:03 +0200

    Migrate `CLI`

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+const config = {
+  presets: ['@babel/preset-env'],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-export-default-from",
+    "ramda"
+  ]
+}
+
+module.exports = config
