a90d0fd8caa75fd2094fc7223aab5c24e28df05c
Denis Davidyuk <denis_davidyuk@hotmail.com>
2022-07-05 14:32:35 +0300

    refactor(nonceArgument): remove unnecessary default, more strict parsing

--- src/arguments.js
+++ src/arguments.js
@@ -3,8 +3,7 @@ import { GAS_MAX } from '@aeternity/aepp-sdk';
 import { NODE_URL } from './utils/constant';
 
 export const nonceArgument = new Argument('<nonce>', 'Unique number that is required to sign transaction securely')
-  .argParser((nonce) => parseInt(nonce))
-  .default(NaN);
+  .argParser((nonce) => +nonce);
 
 export const nodeOption = new Option('-u, --url [hostname]', 'Node to connect to')
   .default(NODE_URL, 'Aeternity testnet');
